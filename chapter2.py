9c600e6bec4c66f25b4ca497c98d1f5039e73157
wikilike7 <wikilike7@gmail.com>
2017-12-13 09:04:18 +0800

    update chapter 2

--- chapter2.py
+++ chapter2.py
@@ -3,7 +3,7 @@ otherString = 'The word "python" usually refers to a snake'
 tripleString = """Here's another way to embed "quotes" in a string"""
 
 
-my_string = 'abc'
+my_string = 'hello, world'
 
 # string concatenation
 string_one = 'My dog ate '
@@ -14,5 +14,34 @@ print(string_three)
 
 # string method
 print(my_string.upper())
-print('hello, world'.upper())
-print(dir(my_string))
+# print('hello, world'.upper())
+print(dir(my_string))  # list all of methods from string
+
+# string slicing
+print(my_string[0:2])
+
+
+# string formatting
+your_string = 'I like %s' % 'Python'
+print(your_string)
+var = 'cookies'
+another_string = '%s and %s' % (your_string, var)
+print(another_string)
+
+
+# calc
+this_string = '%i + %i = %i' % (1, 2, 3)
+print(this_string)
+
+float_string = '%.2f' % 1.23456
+print(float_string)
+
+
+# string templates
+print('%(lang)s is fun' % {'lang': 'Python'})
+
+print('%(value)s %(value)s %(value)s' % {'value': 'spam'})
+
+
+
+
